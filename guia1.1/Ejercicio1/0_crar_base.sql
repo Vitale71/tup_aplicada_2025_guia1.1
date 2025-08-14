395eee778e7a6960d5422b5eb413a274551eaefc
lucasvitale71 <lucasvitale71@gmail.com>
2025-08-14 15:04:40 -0300

    .

---
+++ guia1.1/Ejercicio1/0_crar_base.sql
@@ -0,0 +1,20 @@
+﻿
+
+USE Master
+
+GO
+
+DROP DATABASE IF EXISTS Guia1_1_Ejercicio_lucas_db
+
+GO
+
+CREATE DATABASE Guia1_1_Ejercicio_lucas_db
+
+GO
+
+USE Guia1_1_Ejercicio_lucas_db
+
+
+GO
+
+USE master
---
+++ guia1.1/Ejercicio1/1_crar_tabla.sql
@@ -0,0 +1,32 @@
+﻿USE Guia1_1_Ejercicio_lucas_db
+
+GO
+
+CREATE TABLE Personas
+(
+ Id INT PRIMARY KEY IDENTITY (1,1),
+ LU INT NOT NULL UNIQUE ,
+ Nombre VARCHAR(100) NOT NULL DEFAULT 'sin definir', 
+ Nota DECIMAL(18,2) NOT NULL DEFAULT 0
+)
+
+GO 
+
+INSERT INTO Personas (LU)
+VALUES 
+(3457),
+(2540),
+(4233),
+(2412),
+(2432),
+(8445),
+(9564)
+
+go 
+
+UPDATE Personas SET Nombre = 'Daniela'
+WHERE Id = 1
+
+GO
+
+SELECT * FROM Personas
